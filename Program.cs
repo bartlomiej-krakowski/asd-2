94d346655d958598229c31e62527812857efdf88
Bartłomiej Krakowski <48633090+bartlomiej-krakowski@users.noreply.github.com>
2019-05-15 17:10:13 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,22 +9,49 @@ namespace ConsoleApplication1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            int count = 1;
+
+            Console.WriteLine("Lp;      T;      Result;");
             BigInteger[] array = { 100913, 1009139, 10091401, 100914061, 1009140611, 10091406133, 100914061337, 1009140613399 };
 
+            for (int i = 0; i < array.Length; i++)
+            {
+                var time = new System.Diagnostics.Stopwatch();
+                time.Start();
+
+                isPrime(array[i]);
+                time.Stop();
+
+                long timeDiff = time.ElapsedTicks;
+
+                Console.WriteLine((array[i]) + ";" + time.ElapsedTicks + ";" + isPrimeInstr(array[i], count));
+            }
         }
 
-
-        bool isPrime(BigInteger[] Num)
+        static bool isPrime(BigInteger Num)
         {
             if (Num < 2) return false;
-            else if (Num % 2 == 0) return true;
+            else if (Num < 4) return true;
+            else if (Num % 2 == 0) return false;
             else
                 for (BigInteger u = 3; u < Num / 2; u += 2)
+                {
                     if (Num % u == 0) return false;
+                }
+
             return true;
         }
 
+        static int isPrimeInstr(BigInteger Num, int count)
+        {
+            for (BigInteger u = 3; u < Num / 2; u += 2)
+            {
+                count++;
+            }
+            return count;
+        }
     }
 }
