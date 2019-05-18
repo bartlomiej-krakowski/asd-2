29ff2cc214ce7f3ec25ff2e451ae2906b8afe097
Bartłomiej Krakowski <48633090+bartlomiej-krakowski@users.noreply.github.com>
2019-05-18 21:27:23 +0200

    Create Program-decent.cs

---
+++ Program-decent.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int count = 1;
+
+            Console.WriteLine("Lp;      T;      Result;");
+            BigInteger[] array = { 100913, 1009139, 10091401, 100914061, 1009140611, 10091406133, 100914061337, 1009140613399 };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                var time = new System.Diagnostics.Stopwatch();
+                time.Start();
+
+                isPrime(array[i]);
+                time.Stop();
+
+                Console.WriteLine((array[i]) + ";" + time.ElapsedTicks + ";" + isPrimeInstr(array[i], count));
+            }
+        }
+
+        static bool isPrime(BigInteger Num)
+        {
+            if (Num < 2) return false;
+            else if (Num < 4) return true;
+            else if (Num % 2 == 0) return false;
+            else
+                for (BigInteger u = 3; u * u <= Num; u += 2)
+                {
+                    if (Num % u == 0) return false;
+                }
+
+            return true;
+        }
+
+        static int isPrimeInstr(BigInteger Num, int count)
+        {
+            for (BigInteger u = 3; u * u <= Num; u += 2)
+            {
+                count++;
+            }
+            return count;
+        }
+    }
+}
